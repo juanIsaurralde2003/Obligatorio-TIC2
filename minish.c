c5c08a0e32a0f7358d22a072add36fe7c7652367
saguirregaray1 <saguirregaray1@correo.um.edu.uy>
2023-05-28 17:23:43 -0300

    help, pid y uid

--- minish.c
+++ minish.c
@@ -9,27 +9,56 @@
 
 void malloc_for_list(char **argv) {
     for (int i = 0; i < MAXNUMBERWORDS; i++) {
-        argv[i] = (char *)malloc((MAXWORD + 1) * sizeof(char));
+        argv[i] = (char *)malloc_or_exit((MAXWORD+1) * sizeof(char));
     }
 }
 
-void free_list(char **argv) {
-    for (int i = 0; i < MAXNUMBERWORDS; i++) {
+void free_list(char **argv,int argc){
+    for (int i = 0; i < argc; i++) {
         free(argv[i]);
     }
 }
 
+int last_command_status; 
+
 int main(void){
-    char **argv = (char **)malloc_or_exit(MAXNUMBERWORDS+1);
-    char *buffer = (char *)malloc_or_exit(MAXLINE+1);
-    malloc_for_list(argv);
-    while(1){
-        if(fgets(buffer,MAXLINE,stdin)!=NULL){
-            printf("%d\n",linea2argv(buffer,MAXNUMBERWORDS,argv));
-        }
-        
+    char **argv = (char **)malloc_or_exit((MAXNUMBERWORDS+1)*sizeof (char *));
+    char *buffer = (char *)malloc_or_exit((MAXLINE+1) * sizeof (char));
+    last_command_status = 0;
+
+    while (1) {
+        printf("Shell > ");
+        fgets(buffer, MAXLINE, stdin);
+        malloc_for_list(argv);
+        int argc = linea2argv(buffer, MAXNUMBERWORDS, argv);
+
+   
+        if (argc > 0) {
+            printf("%s\n",argv[0]);
+            char *command = argv[0];
+         
+            if (strcmp(command, "exit") == 0) {
+                builtin_exit(argc,argv);
+                free_list(argv,argc);
+                break;
+            } else if (strcmp(command, "help") == 0) {
+                last_command_status = builtin_help(argc,argv);
+            } else if (strcmp(command, "pid") == 0) {
+                last_command_status = builtin_pid();
+            } else if (strcmp(command, "uid") == 0){
+                last_command_status = builtin_uid();
+            } else if (strcmp(command, "getenv") == 0){
+                
+            } else if (strcmp(command, "setenv") == 0){
+                
+            } 
+            else {
+                printf("Comando no reconocido\n");
+            }
+        }    
+        free_list(argv,argc);
     }
-    free_list(argv);
+  
     free(argv);
     free(buffer);
     return 0;
--- minish_func.c
+++ minish_func.c
@@ -1,10 +1,55 @@
 #include "minish.h"
 #include "wrappers.h"
+#include <sys/types.h>
+#include <unistd.h>
+#include <pwd.h>
 
-int builtin_exit (int argc, char ** argv);
-int builtin_help (int argc, char ** argv);
-int builtin_pid (int argc, char ** argv);
-int builtin_uid (int argc, char ** argv);
+extern int last_command_status; 
+
+int builtin_exit (int argc, char ** argv)
+{
+    if (argc>1){
+        int status = atoi(argv[1]);
+        exit(status);
+    }
+    else{
+        exit(last_command_status);
+    }
+}
+
+int builtin_help (int argc, char ** argv)
+{
+    if (argc>1){
+        //ni idea
+        printf("%s",argv[1]);
+    }
+    else{
+        printf("These shell commands are defined internally. Type `help' to see this list");
+        printf("Type `help name' to find out more about the function `name'");
+        printf("Here's the list of commands: help, exit, lookup, history, status, cd, dir, getenv, set env, pid, uid, unsetenv, ejecutar y externo\n");
+    }
+    return 0;
+}
+int builtin_pid (){
+    pid_t pid = getpid();
+    printf("parent process id is %d\n",(unsigned) pid);
+    return 0;
+}
+
+int builtin_uid (){
+    uid_t uid = geteuid();
+    struct passwd *access = getpwuid(uid);
+    if (access)
+        {
+            printf("nombre: %s\n",access->pw_name);
+            printf("id: %d",uid);
+            return 0;
+        }
+    else{
+            fprintf (stderr,"cannot find username for UID %u\n",(unsigned) uid);
+            return 1;
+        }
+}
 int builtin_getenv (int argc, char ** argv);
 int builtin_setenv (int argc, char ** argv);
 
@@ -27,7 +72,8 @@ int linea2argv(char *linea, int argc, char **argv){
         }
         i++;
     }
-    argv[word_count] = '\0';
-    if (j>0) word_count++;
+    if (j>0 && is_a_space!=1) word_count++;
+    argv[word_count] = NULL;
+
     return word_count;
 }
